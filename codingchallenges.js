4d4eda0685709eaa3fb05ecbfa863662a9b74e7e
Earl Syx Hernandez <hernandezearlsyxmail@gmail.com>
2025-06-10 07:05:53 +0800

    coding challenges 1

---
+++ codingchallenges.js
@@ -0,0 +1,22 @@
+/*
+Mark and John are trying to compare their BMI (Body Mass Index), which is 
+calculated using the formula: 
+BMI = mass / height ** 2 = mass / (height * height) (mass in kg 
+and height in meter). 
+*/
+
+// 1. Store Mark's and John's mass and height in variables
+let markMass = 78;
+let markHeight = 1.69;
+
+let johnMass = 92;
+let johnHeight = 1.95;
+
+/*2. Calculate both their BMIs using the formula (you can even implement both 
+versions) */
+
+let markBMI = markMass / markHeight ** 2;
+let johnBMI = johnMass / johnHeight ** 2;
+
+const markHeigherBMI = markBMI > johnBMI;
+console.log(markHeigherBMI);

7f2ba504a1571027a523d792be09373d47b91c2f
Earl Syx Hernandez <hernandezearlsyxmail@gmail.com>
2025-06-10 06:21:55 +0800

    Basic Operators

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ let myFirstJob = "Data Analyst";
 let myCurrentJob = "Programmer";
 
 console.log(myFirstJob);
-*/
+
 // let javaScriptIsFun = true;
 // console.log(javaScriptIsFun);
 
@@ -57,3 +57,35 @@ job = "teacher";
 //wrong create global variable
 lastName = "test";
 console.log(lastName);
+*/
+
+// Math Operators
+const now = 2023;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas, ageSarah);
+
+console.log(ageJonas * 2, ageSarah / 10, 2 ** 3);
+// 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2;
+
+const firstName = "Jonas";
+const lastName = "Schememe";
+console.log(firstName + " " + lastName);
+
+//Assignment operators
+let x = 10 + 5; //15
+x += 10; // 25
+x *= 4; // 100
+x++; // 101
+x--; //100
+x--; //99;
+console.log(x);
+
+//Comparison Operators
+
+console.log(ageJonas > ageSarah); // > < >= <=
+console.log(ageSarah >= 18);
+
+const isFulleAge = ageSarah >= 18;
+
+console.log(now - 1991 > now - 2018);

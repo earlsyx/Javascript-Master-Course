cdfa50ac852ec28e79a25dc0bd33630a80deab0d
Earl Syx Hernandez <hernandezearlsyxmail@gmail.com>
2025-06-10 06:54:57 +0800

    Assignment, Datat types, let const var, basic operators

--- assignment.js
+++ assignment.js
@@ -1,6 +1,26 @@
 // Values and Variables
-let country = "Philippines";
-let continent = "Asia";
+const country = "Philippines";
+const continent = "Asia";
 let population = 174000000;
 
 console.log(country, continent, population);
+
+// Data Types;
+const isIsland = true;
+let language;
+
+console.log(isIsland, population, country, language);
+
+// let , const, var
+language = "Filipino";
+
+// Basic Operators
+population / 2 / 2;
+population++;
+console.log(population);
+let finlandPopulation = 6000000;
+population > finlandPopulation;
+let averagePopulation = 330000000;
+population < averagePopulation;
+let description =
+  "Portugal is in Europe, and its 11 million people speaks portuguese";

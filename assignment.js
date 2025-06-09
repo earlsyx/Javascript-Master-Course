48a5a56ca7596b3d67c5760b3b571c1eaf9e6550
Earl Syx Hernandez <hernandezearlsyxmail@gmail.com>
2025-06-10 05:52:30 +0800

    Data Types

---
+++ assignment.js
@@ -0,0 +1,6 @@
+// Values and Variables
+let country = "Philippines";
+let continent = "Asia";
+let population = 174000000;
+
+console.log(country, continent, population);
--- script.js
+++ script.js
@@ -1,7 +1,42 @@
+/*
 let js = "amazing";
-
-if (js === "amazing") alert("Javascript is Fun!");
-
-48 + 41 + 500 - 25;
-
 console.log(48 + 41 + 500 - 25);
+
+console.log("Earl");
+console.log(6);
+let firstName = "Earl";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let earl_hehe = "Idk";
+let $function = 26;
+
+let person = "Earl";
+let PI = 3.1415;
+
+let myFirstJob = "Data Analyst";
+let myCurrentJob = "Programmer";
+
+console.log(myFirstJob);
+*/
+let javaScriptIsFun = true;
+console.log(javaScriptIsFun);
+
+// console.log(typeof true);
+console.log(typeof javaScriptIsFun);
+
+// console.log(typeof 23);
+// console.log(typeof "Jonas");
+
+javaScriptIsFun = "Yes";
+console.log(typeof javaScriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null);

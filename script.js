457a07ab6ad7f8cedd1464dc6b04eb826b426429
Earl Syx Hernandez <hernandezearlsyxmail@gmail.com>
2025-06-23 20:40:54 +0800

    Truthy and falsy values

--- script.js
+++ script.js
@@ -155,3 +155,25 @@ console.log(century);
 // 3.23
 // 4.
 // to continue
+
+// 5 falsy values: 0, '', undefined, null, NaN
+
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean("Jonas"));
+console.log(Boolean({}));
+console.log(Boolean(""));
+
+const money = 0;
+if (money) {
+  console.log("Dont spend it all");
+} else {
+  console.log("You shoud get a Job!");
+}
+
+let height;
+if (height) {
+  console.log("Height is defined");
+} else {
+  console.log("Height is undefiend");
+}

f2c6f2d15b981d0e205a8baed6735cba7b40cfa7
Earl Syx Hernandez <hernandezearlsyxmail@gmail.com>
2025-06-24 08:40:45 +0800

    Equality operator loose and strict

--- script.js
+++ script.js
@@ -158,22 +158,45 @@ console.log(century);
 
 // 5 falsy values: 0, '', undefined, null, NaN
 
-console.log(Boolean(0));
-console.log(Boolean(undefined));
-console.log(Boolean("Jonas"));
-console.log(Boolean({}));
-console.log(Boolean(""));
+// console.log(Boolean(0));
+// console.log(Boolean(undefined));
+// console.log(Boolean("Jonas"));
+// console.log(Boolean({}));
+// console.log(Boolean(""));
 
-const money = 0;
-if (money) {
-  console.log("Dont spend it all");
+// const money = 0;
+// if (money) {
+//   console.log("Dont spend it all");
+// } else {
+//   console.log("You shoud get a Job!");
+// }
+
+// let height;
+// if (height) {
+//   console.log("Height is defined");
+// } else {
+//   console.log("Height is undefiend");
+// }
+
+const ages = 19;
+if (age === 18) console.log("You just became an adult :D");
+
+//strict and loose, type coersion type conversion
+
+if (age == 18) console.log("You just became an adult :D (loose");
+
+const favourite = Number(prompt("What's your favourite number?"));
+console.log(favourite);
+console.log(typeof favourite);
+
+if (favourite === 23) {
+  console.log("Cool! 23 is an amazing number!");
+} else if (favourite === 7) {
+  console.log("7 is also a cool number");
+} else if (favourite === 9) {
+  console.log("9 is also a cool number");
 } else {
-  console.log("You shoud get a Job!");
+  console.log("Number is not 23 or 7");
 }
 
-let height;
-if (height) {
-  console.log("Height is defined");
-} else {
-  console.log("Height is undefiend");
-}
+if (favourite !== 23) console.log("Why not 23");
